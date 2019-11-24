cb34253cbf5e3284c79d06d24b693fa90e159183
buptroot <lwfplus@gmail.com>
2019-11-24 20:54:09 +0800

    添加heap

--- Sorting-Basic/myalgo/SelectionSort.java
+++ Sorting-Basic/myalgo/SelectionSort.java
@@ -1,26 +1,26 @@
 package myalgo;
 
 public class SelectionSort {
-    private SelectionSort(){};
-    public static void sort(int[] arr){
+    private SelectionSort(){}
+    public static void sort(Comparable[] arr){
         int len=arr.length;
         for(int i=0;i<len;i++){
             int minidx=i;
             for(int j=i+1;j<len;j++)
-                if(arr[j]<arr[minidx])
+                if(arr[j].compareTo(arr[minidx])<0)
                     minidx=j;
             swap(arr,i,minidx);
         }
     }
 
-    private static void swap(int[] arr, int i, int minidx) {
-        int tmp=arr[i];
+    private static void swap(Object[] arr, int i, int minidx) {
+        Object tmp=arr[i];
         arr[i]=arr[minidx];
         arr[minidx]=tmp;
     }
 
     public static void main(String[] args) {
-        int[] arr = {10,9,8,7,6,5,4,3,2,1};
+        Integer[] arr = {10,9,8,7,6,5,4,3,2,1};
         SelectionSort.sort(arr);
         for (int value : arr) {
             System.out.print(value);
